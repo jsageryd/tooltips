ac8682e6dd187dc4082a3eb3d737530395471e61
Johan Sageryd <j416@1616.se>
2010-06-07 18:48:36 +0200

    Remove whitespace

--- tooltips.php
+++ tooltips.php
@@ -54,7 +54,7 @@ function sectionstohash(&$string, $sectionidentifier){
 	}
 
 //	Remove all |sectionidentifier| |/sectionidentifier| sections from the original string
-	do{	
+	do{
 		$laststring = $string;
 		$string = preg_replace("/(\|$sectionidentifier\|.*\|\/$sectionidentifier\|\s)/uisU", '', $string);
 	}while($string !== $laststring);
